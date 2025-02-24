24841069a1a34c371e7c95d7c037dcd407f2e284
Wuerfele-Wyatt <99354113+Wuerfele-Wyatt@users.noreply.github.com>
2025-02-24 12:12:50 -0600

    Finished setup
    Set up new JafaFX project and tested creating a scene without the default scene

--- src/main/java/edu/missouriwestern/wwuerfele/homework04/HelloApplication.java
+++ src/main/java/edu/missouriwestern/wwuerfele/homework04/HelloApplication.java
@@ -3,6 +3,7 @@ package edu.missouriwestern.wwuerfele.homework04;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,10 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Pane root = new Pane();
+
+        stage.setTitle("yeeeOUCHHHHHH");
+        Scene primaryScene = new Scene(root, 500, 200);
 
 
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        stage.setScene(primaryScene);
         stage.show();
     }
 
